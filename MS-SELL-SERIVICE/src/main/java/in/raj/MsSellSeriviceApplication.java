196b245f7e9894343d1f99b5e7b53555a318d52b
rajukumar59210@gmail.com <rajukumar59210@gmail.com>
2024-09-23 17:28:59 +0530

    sell service has begin

---
+++ MS-SELL-SERIVICE/src/main/java/in/raj/MsSellSeriviceApplication.java
@@ -0,0 +1,13 @@
+package in.raj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSellSeriviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSellSeriviceApplication.class, args);
+	}
+
+}
---
+++ MS-SELL-SERIVICE/src/test/java/in/raj/MsSellSeriviceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.raj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSellSeriviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
